ab3ed09d66b667fc31795eda754a4374ca899c4b
Christian Fleury <chfleurysiq@gmail.com>
2021-11-10 20:36:52 -0300

    feat: ensure BcryptAdapter calls bcrypt with valid values

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,4 +1,4 @@
-const config = require('./jest.config');
-config.testMatch = ['**/*.test.ts'];
+const config = require('./jest.config')
+config.testMatch = ['**/*.test.ts']
 
-module.exports = config;
+module.exports = config

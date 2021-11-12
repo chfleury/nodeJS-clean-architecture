abfa44a5e36a8307497a2e59c6a4e71683b686af
Christian Fleury <chfleurysiq@gmail.com>
2021-11-12 12:47:25 -0300

    chore: add coverage config

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
 
-  collectCoverageFrom: ['<rootDir>/src/**/*ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
 
   coverageProvider: 'v8',

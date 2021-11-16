3252bb74f0e86ac5deaef3b3eb529a89deccc009
Christian Fleury <chfleurysiq@gmail.com>
2021-11-16 16:20:39 -0300

    refactor: remove unnecessary code

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -46,11 +46,7 @@ export class SingUpController implements Controller {
 
       return ok(acc)
     } catch (err: any) {
-      const error = new Error()
-      error.stack = err.stack
-      error.name = err.name
-      error.message = err.message
-      return serverError(error)
+      return serverError(err)
     }
   }
 }

a7713d2a3b54a6d8011ee3d7fe0f92d369733c11
Christian Fleury <chfleurysiq@gmail.com>
2021-11-18 20:20:16 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 with invalid values provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'ch@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })

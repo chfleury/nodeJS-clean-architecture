2a3aec5e06ba2db90af7b42a10dc2d89d4ecb8f7
Christian Fleury <chfleurysiq@gmail.com>
2021-11-18 17:32:15 -0300

    feat: ensure acessToken is updated on updateAcessToken sucess

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -22,4 +22,13 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
     return MongoHelper.map(result._id.toString(), result)
   }
+
+  async updateAcessToken (id: string, token: string): Promise<void> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: id }, {
+      $set: {
+        acessToken: token
+      }
+    })
+  }
 }
